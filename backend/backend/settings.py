40399740c5ebb915d14b6514ebf868ea0bb5c09d
Vyacheslav Gizov <vogizov@gmail.com>
2024-10-20 17:21:52 +0500

    secret_key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,14 +3,13 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY'),
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
 ALLOWED_HOSTS = [
     '51.250.18.68', 'localhost', '127.0.0.1', 'taski-fromslavka.hopto.org'
 ]
-
 
 
 INSTALLED_APPS = [
