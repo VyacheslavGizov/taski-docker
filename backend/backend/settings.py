8b0c7fccade604b94219ff368f712736a0f82f04
Vyacheslav Gizov <vogizov@gmail.com>
2024-10-23 21:42:34 +0500

    Taski

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,15 +13,6 @@ DEBUG = bool(os.getenv('DEBUG'))
 
 ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS')).split()
 
-# SECRET_KEY = str(os.getenv('SECRET_KEY'))
-# # SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
-
-# DEBUG = True
-
-# ALLOWED_HOSTS = [
-#     '51.250.18.68', 'localhost', '127.0.0.1', 'taski-fromslavka.hopto.org'
-# ]
-
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'django.contrib.admin',
